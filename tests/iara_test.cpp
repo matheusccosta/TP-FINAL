94a23ce4835177b8a642c6ee2c7c036a1d176c78
Matheus Castro Costa <matheuscastrocosta@ufmg.br>
2022-12-06 19:10:02 -0300

    Teste Curupira

--- tests/iara_test.cpp
+++ tests/iara_test.cpp
@@ -6,23 +6,20 @@
 TEST_CASE("TESTANDO ATAQUE"){
 
 std::string NomeJ="test";
-Personagem *p1 = new Curupira (NomeJ);
-Personagem *p2 = new Iara (NomeJ);
+Personagem *p1 = new Iara (NomeJ);
+Personagem *p2 = new Curupira (NomeJ);
 Personagem *p3 = new Saci (NomeJ);
 Personagem *p4 = new Mula (NomeJ);
 
 //1.7
 
 
-SUBCASE("EFETIVIDADE DO CURUPIRA"){
+SUBCASE("EFETIVIDADE DA IARA"){
 
     SUBCASE("CONTRA SACI E IARA"){
 
         CHECK(p1->Ataque(p2, 0.6) == (int) (1.7*(18-p2->getDefesa())));
         CHECK(p1->Ataque(p2, 0.7) == (int) (1.7*(21-p2->getDefesa())));
-
-        CHECK(p1->Ataque(p3, 0.8) == (int) (1.7*(24-p3->getDefesa())));
-        CHECK(p1->Ataque(p3, 0.9) == (int) (1.7*(27-p3->getDefesa())));
         
         CHECK(p1->Ataque(p3, 0.9) == 27-p3->getDefesa());
         }
